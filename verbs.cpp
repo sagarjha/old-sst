9850cb7c679be4bc66a2b40756f95724869ddd10
Edward Tremel <edward@cs.cornell.edu>
2016-05-03 15:08:53 -0400

    Reduced CQ poll timeout to 20 ms

--- verbs.cpp
+++ verbs.cpp
@@ -52,8 +52,8 @@ void check_for_error (T var, string msg) {
 }
 
 namespace sst {
-  /** Completion Queue poll timeout in millisec (2 seconds) */
-  const int MAX_POLL_CQ_TIMEOUT = 2000;
+  /** Completion Queue poll timeout in millisec */
+  const int MAX_POLL_CQ_TIMEOUT = 20;
   /** IB device name. */
   const char *dev_name = NULL;
   /** Local IB port to work with. */
