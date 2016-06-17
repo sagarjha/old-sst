37c57fcfecc1332bf8c1316d53b94a563d072a1b
Sagar Jha <sjsagarjha3@gmail.com>
2016-06-17 16:41:49 -0400

    Formatted the files using clang-format-3.6. Added custom target format_sst in CMakeLists.txt

--- named_function.h
+++ named_function.h
@@ -8,7 +8,6 @@
 
 namespace sst {
 
-
 /**
  * Represents a named function over an SST; the function should take a
  * reference to SST as its argument, and have a name chosen from a user-defined
@@ -21,30 +20,29 @@ namespace sst {
  * @tparam Ret The return type of this function, which must be POD because it
  * will be stored in an SST row.
  */
-template<typename NameEnum, NameEnum Name, typename Param, typename Ret>
+template <typename NameEnum, NameEnum Name, typename Param, typename Ret>
 struct NamedFunction {
     static_assert(std::is_pod<Ret>::value, "Error: only POD return types");
-    Ret (*fun) (const Param&);
+    Ret (*fun)(const Param &);
     using ret_t = Ret;
     static constexpr NameEnum name = Name;
 };
-	
 
 /**
- * Helper for the "make_named_function" macro, which constructs a named function.
+ * Helper for the "make_named_function" macro, which constructs a named
+ * function.
  * This helps hide the template parameters.
  * @param fun A pointer to the function to name.
  * @return A NamedFunction wrapping the given function.
  */
-template<typename NameEnum, NameEnum Name, typename Param, typename Ret>
-auto build_named_function(Ret (*fun)(const Param&) ){
-    return NamedFunction<NameEnum,Name,Param,Ret>{fun};
+template <typename NameEnum, NameEnum Name, typename Param, typename Ret>
+auto build_named_function(Ret (*fun)(const Param &)) {
+    return NamedFunction<NameEnum, Name, Param, Ret>{fun};
 }
-	template<typename NameEnum, NameEnum Name, typename F>
-	auto buid_named_function(F f){
-		return build_named_function(util::convert_fp(f));
-	}
-
+template <typename NameEnum, NameEnum Name, typename F>
+auto buid_named_function(F f) {
+    return build_named_function(util::convert_fp(f));
+}
 
 /**
  * Constructs a NamedFunction, using the first argument as the name and the
@@ -55,8 +53,8 @@ auto build_named_function(Ret (*fun)(const Param&) ){
  * @return A NamedFunction wrapping the given function and associating it with
  * the given name
  */
-#define make_named_function(name,fun...) build_named_function<decltype(name),name>(fun)
-
+#define make_named_function(name, fun...) \
+    build_named_function<decltype(name), name>(fun)
 }
 
 #endif /* NAMED_FUNCTION_H_ */
