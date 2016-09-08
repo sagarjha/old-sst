b939ebcf5adeea7bd9b29444075c705b007099ec
Sagar Jha <sjsagarjha3@gmail.com>
2016-09-08 18:27:21 -0400

    Changes to multicast latency.

--- experiments/multicast_latency.cpp
+++ experiments/multicast_latency.cpp
@@ -4,7 +4,6 @@
 
 #include "multicast.h"
 #include "../../max_members.h"
-#include "time_skew.h"
 
 using namespace std;
 using namespace sst;
@@ -37,20 +36,24 @@ int main() {
         members[i] = i;
     }
 
-    vector<vector<long long int>> times(MAX_MEMBERS);
+    vector<vector<int64_t>> recv_times(MAX_MEMBERS);
     for(uint i = 0; i < MAX_MEMBERS; ++i) {
-        times[i].resize(num_messages);
+        recv_times[i].resize(num_messages);
     }
+
+
+    vector<int64_t> send_times(num_messages);
+
     uint num_finished = 0;
-    struct timespec time;
+    struct timespec recv_time, send_time;
     group<window_size, max_msg_size, MAX_MEMBERS> g(
         members, node_id,
-        [&times, &time, &num_finished, &num_nodes, &num_messages](
+        [&recv_times, &recv_time, &num_finished, &num_nodes, &num_messages](
             uint32_t sender_rank, uint64_t index, volatile char* msg,
             uint32_t size) {
             // start timer
-            clock_gettime(CLOCK_REALTIME, &time);
-            times[sender_rank][index] = time.tv_sec * 1e9 + time.tv_nsec;
+            clock_gettime(CLOCK_REALTIME, &recv_time);
+            recv_times[sender_rank][index] = recv_time.tv_sec * 1e9 + recv_time.tv_nsec;
             if(index == num_messages - 1) {
                 num_finished++;
             }
@@ -62,42 +65,32 @@ int main() {
         volatile char* buf;
         while((buf = g.get_buffer(max_msg_size)) == NULL) {
         }
+	clock_gettime(CLOCK_REALTIME, &send_time);
+	send_times[i] = send_time.tv_sec * 1e9 + send_time.tv_nsec;
         g.send();
     }
     while(!done) {
     }
-    uint64_t skew = 0;
-    if(node_id != 0) {
-        SST<TimeRow> time_sst({0, node_id}, node_id);
-        time_sst[0].time_in_nanoseconds = -1;
-        time_sst[1].time_in_nanoseconds = -1;
-        time_sst.sync_with_members();
-        skew = server(time_sst, 1, 0);
-        time_sst.sync_with_members();
-    }
-    else {
-      for (uint i = 1; i < num_nodes; ++i) {
-          SST<TimeRow> time_sst({0, i}, 0);
-          time_sst[0].time_in_nanoseconds = -1;
-          time_sst[1].time_in_nanoseconds = -1;
-          time_sst.sync_with_members();
-          client(time_sst, 0, 1);
-          time_sst.sync_with_members();
-      }
-    }
     
     for(uint i = 0; i < MAX_MEMBERS; ++i) {
         stringstream ss;
-        ss << MAX_MEMBERS << "_" << i;
+        ss << "ml_" << MAX_MEMBERS << "_" << i;
         ofstream fout;
         fout.open(ss.str());
         for(uint j = 0; j < num_messages; ++j) {
-            fout << times[i][j]-skew << endl;
+            fout << recv_times[i][j] << endl;
         }
         fout.close();
     }
 
-    cout << "skew is " << skew << endl;
+    stringstream ss;
+    ss << "ml_" << MAX_MEMBERS;
+    ofstream fout;
+    fout.open(ss.str());
+    for(uint i = 0; i < num_messages; ++i) {
+        fout << send_times[i] << endl;
+    }
+    fout.close();
 
     for(uint i = 0; i < num_nodes; ++i) {
         if(i == node_id) {
